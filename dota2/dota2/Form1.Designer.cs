bb86ebe996985f0676a39c4474902b5a94ee1e48
Monluxfer <shibarome@gmail.com>
2020-11-28 10:59:31 +0300

    add dota2 project

---
+++ dota2/dota2/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace dota2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkedListBox2 = new System.Windows.Forms.CheckedListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "pudge",
+            "lion",
+            "doom",
+            "windranger",
+            "orge magi",
+            "lone druid",
+            "faceless void",
+            "invoker",
+            "legion commander",
+            "monkey king",
+            "beastmaster",
+            "sven",
+            "vengerful spirit",
+            "dark willow",
+            "broodmother",
+            "visage",
+            "outworld devourer",
+            "treant protector",
+            "meduse",
+            "mirana",
+            "shadow fiend",
+            "arc warden",
+            "earthshaker",
+            "axe",
+            "enchantress",
+            "wraith king",
+            "enigma"});
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 44);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(239, 409);
+            this.checkedListBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(180, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Выберите 5 персонажей команды";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(604, 44);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(399, 394);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(604, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Прямой проход";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(827, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Обратный проход";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkedListBox2
+            // 
+            this.checkedListBox2.FormattingEnabled = true;
+            this.checkedListBox2.Items.AddRange(new object[] {
+            "союзник pudge",
+            "союзник lion",
+            "союзник doom",
+            "союзник windranger",
+            "союзник orge magi",
+            "союзник lone druid",
+            "союзник faceless void",
+            "союзник invoker",
+            "союзник legion commander",
+            "союзник monkey king",
+            "союзник beastmaster",
+            "союзник sven",
+            "союзник vengerful spirit",
+            "союзник dark willow",
+            "союзник broodmother",
+            "союзник visage",
+            "союзник outworld devourer",
+            "союзник treant protector",
+            "союзник meduse",
+            "союзник mirana",
+            "союзник shadow fiend",
+            "союзник arc warden",
+            "союзник earthshaker",
+            "союзник axe",
+            "союзник enchantress",
+            "союзник wraith king",
+            "союзник enigma"});
+            this.checkedListBox2.Location = new System.Drawing.Point(274, 44);
+            this.checkedListBox2.Name = "checkedListBox2";
+            this.checkedListBox2.Size = new System.Drawing.Size(239, 409);
+            this.checkedListBox2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(274, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(232, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Возможно ли получить для своей команды?";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(729, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Очистить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1015, 465);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkedListBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckedListBox checkedListBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ dota2/dota2/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dota2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static HashSet<string> Forward(HashSet<string> start_facts, List<Rule> rules, ref TextBox textBox, string fin)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                foreach (var rule in rules)
+                {
+                    var needed_facts = new HashSet<string>(rule.left);
+                    var new_facts = needed_facts.Except(start_facts);
+
+                    if (new_facts.Count() <= 0 && !start_facts.Contains(rule.right))
+                    {
+                        string facts = "";
+                        foreach (var fact in rule.left)
+                        {
+                            facts = facts + fact + " ";
+                        }
+
+                        start_facts.Add(rule.right);
+                        textBox.AppendText(String.Format("{0} -> {1}", facts, rule.right) + System.Environment.NewLine);
+                        if (rule.right == fin)
+                        {
+                            textBox.AppendText("Искомый персонаж найден");
+                            return start_facts;
+                        }
+                    }
+                }
+            }
+            textBox.AppendText("Искомый персонаж не найден");
+            return start_facts;
+        }
+
+        public class CustomTreeNode
+        {
+            public String Label { get; set; }
+            public List<CustomTreeNode> Children { get; set; }
+        }
+
+        public static HashSet<string> Backward(string start_fact, List<Rule> rules, ref TextBox textBox, string fin)
+        {
+            HashSet<string> facts = new HashSet<string>();
+            facts.Add(fin);
+            HashSet<string> timed_facts = new HashSet<string>();
+
+            for (int i = 0; i < 4; i++)
+            {
+                foreach (var rule in rules)
+                {
+                    foreach (var fact in facts)
+                    {
+                        if (fact == rule.right)
+                        {
+                            foreach (var item in rule.left)
+                            {
+                                timed_facts.Add(item);
+                            }
+                        }
+                    }
+
+                    foreach (var fact in timed_facts)
+                    {
+                        facts.Add(fact);
+                    }
+                    timed_facts = new HashSet<string>();
+                }
+            }
+
+            if (facts.Contains(start_fact))
+            {
+                HashSet<string> start_facts = new HashSet<string>() { start_fact };
+                bool is_there_fact = false;
+                for (int i = 0; i < 4; i++)
+                {
+                    foreach (var rule in rules)
+                    {
+                        var needed_facts = new HashSet<string>(rule.left);
+                        var new_facts = needed_facts.Except(start_facts);
+
+                        if (new_facts.Count() <= 0 && !start_facts.Contains(rule.right))
+                        {
+                            string n_facts = "";
+                            foreach (var fact in rule.left)
+                            {
+                                n_facts = n_facts + fact + " ";
+                            }
+
+                            start_facts.Add(rule.right);
+                            if (rule.right == fin)
+                            {
+                                is_there_fact = true;
+                            }
+                        }
+                    }
+                }
+                if (is_there_fact)
+                {
+                    Forward(new HashSet<string>() { start_fact }, rules, ref textBox, fin);
+                }
+                else
+                {
+                    textBox.AppendText("Искомый персонаж не найден" + System.Environment.NewLine);
+                }
+
+            }
+            else
+            {
+                textBox.AppendText("Искомый персонаж не найден" + System.Environment.NewLine);
+            }    
+
+            return facts;
+        }
+
+        public List<Rule> return_rules()
+        {
+            List<Rule> rules = new List<Rule>();
+
+            string line;
+            System.IO.StreamReader file = new System.IO.StreamReader("rules.txt");
+            char[] separators = { ',', '-' };
+            int ind = 0;
+
+            while ((line = file.ReadLine()) != null)
+            {
+                if (line == "")
+                    continue;
+                var string_array = line.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+                Rule rule = new Rule();
+                rules.Add(rule);
+                for (int i = 0; i < string_array.Length - 1; i++)
+                {
+                    rules[ind].left.Add(string_array[i]);
+                }
+
+                rules[ind].right = string_array[string_array.Length - 1];
+                ind++;
+            }
+            file.Close();
+
+            return rules;
+        }
+
+        public List<string> return_team()
+        {
+            string line;
+            List<string> team = new List<string>();
+            System.IO.StreamReader file = new System.IO.StreamReader("team.txt");
+
+            while ((line = file.ReadLine()) != null)
+            {
+                if (line == "")
+                    continue;
+
+                team.Add(line);
+            }
+
+            file.Close();
+
+            return team;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var start_facts = new HashSet<string>();
+            var rules = return_rules();
+
+            foreach (object itemChecked in checkedListBox1.CheckedItems)
+                start_facts.Add(itemChecked.ToString());
+
+            if (start_facts.Count == 5)
+            {
+                var fin_facts = Forward(start_facts, rules, ref textBox1, checkedListBox2.CheckedItems[0].ToString());
+            }
+            else
+            {
+                textBox1.AppendText("В команде соперника должно быть 5 персонажей" + System.Environment.NewLine);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var rules = return_rules();
+
+            var fin_facts = Backward(checkedListBox1.CheckedItems[0].ToString(), rules, ref textBox1, checkedListBox2.CheckedItems[0].ToString());
+        }
+    }
+
+    public class Rule
+    {
+        public List<string> left;
+        public string right;
+
+        public Rule()
+        {
+            left = new List<string>();
+            right = "";
+        }
+
+    }
+}
